160f6ab87d32f7890e3e48d639e69470af0d8bb2
GG79 <midlifegamingcrisis@gmail.com>
2024-01-21 10:59:54 -0800

    Modified function for ps-housing

--- server.lua
+++ server.lua
@@ -1,6 +1,7 @@
 local QBCore = exports['qb-core']:GetCoreObject()
 
-QBCore.Functions.CreateCallback('qb-spawn:server:getOwnedHouses', function(_, cb, cid)
+--removed by pamela for ps-housing
+--[[QBCore.Functions.CreateCallback('qb-spawn:server:getOwnedHouses', function(_, cb, cid)
     if cid ~= nil then
         local houses = MySQL.query.await('SELECT * FROM player_houses WHERE citizenid = ?', {cid})
         if houses[1] ~= nil then
@@ -11,4 +12,19 @@ QBCore.Functions.CreateCallback('qb-spawn:server:getOwnedHouses', function(_, cb
     else
         cb({})
     end
+end)]]
+
+--added by pamela for ps-housing
+QBCore.Functions.CreateCallback('qb-spawn:server:getOwnedHouses', function(_, cb, cid)
+    if cid ~= nil then
+        local houses = MySQL.query.await('SELECT * FROM properties WHERE owner_citizenid = ?', {cid})
+        if houses[1] ~= nil then
+            cb(houses)
+        else
+            cb({})
+        end
+    else
+        cb({})
+    end
 end)
+--till here
